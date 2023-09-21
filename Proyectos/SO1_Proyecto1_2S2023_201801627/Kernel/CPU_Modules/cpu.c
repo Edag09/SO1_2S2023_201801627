13916052b05c8ae5c88ef55fb31bf1e4ddf5bc66
Eduardo Agustin <3006840880101@ingenieria.usac.edu.gt>
2023-09-21 11:44:33 -0600

    Restart

---
+++ Proyectos/SO1_Proyecto1_2S2023_201801627/Kernel/CPU_Modules/cpu.c
@@ -0,0 +1,93 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("CPU Module - Get data for process, PID, name, child and more");
+MODULE_AUTHOR("Eduardo René Agustin Mendoza");
+
+struct task_struct *task;
+struct task_struct *task_child;
+struct list_head *list;
+
+char *name = "cpu_201801627";
+
+static int content_file(struct seq_file *file, void *v) {
+    unsigned long rss;
+
+    seq_printf(file, "{\"process\":[");
+
+    int count_running = 0, count_sleeping = 0, count_zombie = 0, count_stopped = 0;
+
+    for_each_process(task) {
+
+        switch (task->__state) {
+            case TASK_RUNNING:
+                count_running++;
+                break;
+            case EXIT_TRACE:
+            case TASK_DEAD:
+                count_zombie++;
+                break;
+            case TASK_STOPPED:
+                count_stopped++;
+                break;
+            default:
+                count_sleeping++;
+        }
+
+        if (task->mm){
+            rss = get_mm_rss(task->mm) << PAGE_SHIFT;
+            seq_printf(file, "{\"pid\": %d, \"name\": \"%s\", \"user\": %d, \"state\": %i, \"ram\": %lu, \"children\": [", task->pid, task->comm, task->cred->uid, task->__state, rss);
+        } else {
+            seq_printf(file, "{\"pid\": %d, \"name\": \"%s\", \"user\": %d, \"state\": %i, \"ram\": %d, \"children\": [", task->pid, task->comm, task->cred->uid, task->__state, 0);
+        }
+
+        list_for_each(list, &task->children) {
+            task_child = list_entry(list, struct task_struct, sibling);
+
+            if (list->next == &task->children) {
+                seq_printf(file, "{\"pid\": %d, \"name\": \"%s\" }", task_child->pid, task_child->comm);
+            } else {
+                seq_printf(file, "{\"pid\": %d, \"name\": \"%s\" },", task_child->pid, task_child->comm);
+            }
+        }
+
+        if (next_task(task) == &init_task) {
+            seq_printf(file, "]}");
+        } else {
+            seq_printf(file, "]}");
+        }
+    }
+
+    seq_printf(file, "],\"summary\": {\"running\": %d, \"sleeping\": %d, \"stopped\": %d, \"zombie\": %d }", count_running, count_sleeping, count_stopped, count_zombie);
+
+    return 0;
+}
+
+static int get_data(struct inode *inode, struct file *file){
+    return single_open(file, content_file, NULL);
+}
+
+static struct proc_ops data = {
+        .proc_open = get_data,
+        .proc_read = seq_read
+};
+
+static int __init add_module(void) {
+    proc_create(name, 0, NULL, &data);
+    printk(KERN_INFO "Eduardo René Agustin Mendoza\n");
+    return 0;
+}
+
+static void __exit remove_module(void) {
+    remove_proc_entry(name, NULL);
+    printk(KERN_INFO "Segundo Semestre 2023\n");
+}
+
+module_init(add_module);
+module_exit(remove_module);
---
+++ Proyectos/SO1_Proyecto1_2S2023_201801627/Kernel/RAM_Modules/ram.c
@@ -0,0 +1,66 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/seq_file.h>
+
+const long minute = 60;
+const long hour = 60 * minute;
+const long day = 24 * hour;
+const long mb = 1024 * 1024;
+
+struct sysinfo ram;
+char *name = "ram_201801627";
+
+static void init_meminfo(void){
+    si_meminfo(&ram);
+}
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("MODULO RAM LABORATORIO SOPES 1 P1");
+MODULE_AUTHOR("Eduardo Rene Agustin Mendoza");
+
+static int escribir_archivo(struct seq_file *archivo, void *v){
+    init_meminfo();
+    seq_printf(archivo, "{\n\"total\": %ld", (ram.totalram << PAGE_SHIFT));
+    seq_printf(archivo, ", \n");
+    seq_printf(archivo, "\"free\": %ld", (ram.freeram << PAGE_SHIFT));
+    seq_printf(archivo, ", \n");
+    seq_printf(archivo, "\"used\": %ld", ((ram.totalram - ram.freeram) << PAGE_SHIFT)+'\n');
+    seq_printf(archivo, "\n");
+    seq_printf(archivo, "}\n");
+    return 0;
+}
+
+//Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
+static int al_abrir(struct inode *inode, struct file *file)
+{
+    return single_open(file, escribir_archivo, NULL);
+}
+
+//Si el kernel es 5.6 o mayor se usa la estructura proc_ops
+static struct proc_ops operaciones =
+{
+    .proc_open = al_abrir,
+    .proc_read = seq_read
+};
+
+//Funcion a ejecuta al insertar el modulo en el kernel con insmod
+static int _insert(void)
+{
+    proc_create(name, 0, NULL, &operaciones);
+    printk(KERN_INFO "Eduardo Rene Agustin Mendoza - 201801627\n");
+    return 0;
+}
+
+//Funcion a ejecuta al remover el modulo del kernel con rmmod
+static void _remove(void)
+{
+    remove_proc_entry(name, NULL);
+    printk(KERN_INFO "Segundo Semestre 2023 SOPES 1\n");
+}
+
+module_init(_insert);
+module_exit(_remove);
