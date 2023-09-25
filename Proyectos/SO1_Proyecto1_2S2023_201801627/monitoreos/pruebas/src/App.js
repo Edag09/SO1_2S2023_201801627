f391b1c9dcaddeb11039920c9c574d591fc4c55a
Eduardo Agustin <3006840880101@ingenieria.usac.edu.gt>
2023-09-24 23:43:27 -0600

    Pages

--- Proyectos/SO1_Proyecto1_2S2023_201801627/monitoreos/pruebas/src/App.js
+++ Proyectos/SO1_Proyecto1_2S2023_201801627/monitoreos/pruebas/src/App.js
@@ -1,17 +1,39 @@
-import React, { Fragment } from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
+import React, { useEffect, useState } from 'react';
 
-const App = () => {
+function App() {
+  const [data, setData] = useState({
+    total: 0,
+    free: 0,
+    used: 0,
+  });
+
+  useEffect(() => {
+    // Realiza una solicitud GET utilizando fetch
+    fetch('http://localhost:4000/ram')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('La solicitud no fue exitosa');
+        }
+        return response.json(); // Analiza el JSON de la respuesta
+      })
+      .then((json) => {
+        setData(json); // Almacena los datos en el estado
+      })
+      .catch((error) => {
+        console.error('Error al realizar la solicitud:', error.message);
+      });
+  }, []);
+
   return (
-    <Fragment>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </Fragment>
-  )
+    <div>
+      <h1>Información del JSON</h1>
+      <div>
+        <p><strong>Total:</strong> {data.total}</p>
+        <p><strong>Free:</strong> {data.free}</p>
+        <p><strong>Used:</strong> {data.used}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
