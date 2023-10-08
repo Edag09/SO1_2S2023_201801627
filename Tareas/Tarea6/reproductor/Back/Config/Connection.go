8f96830ffea564e1b1415a240367ae8a6e78650a
Eduardo Agustin <3006840880101@ingenieria.usac.edu.gt>
2023-10-07 18:03:27 -0600

    Tarea 6

---
+++ Tareas/Tarea6/reproductor/Back/Config/Connection.go
@@ -0,0 +1,9 @@
+package config
+
+import (
+	"fmt"
+)
+
+func Connect_db() {
+	fmt.Println("Si me conecte :D")
+}
---
+++ Tareas/Tarea6/reproductor/Back/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	config "Back/Config"
+)
+
+func main() {
+	config.Connect_db()
+}
